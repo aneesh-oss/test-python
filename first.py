a60ded83f087d84e832545183f22182eb69f21fd
Aneesh-oss <creadora08@gmail.com>
2024-03-18 17:33:54 +0530

    new commit

--- first.py
+++ first.py
@@ -1,10 +1,11 @@
-def is_student(name):
-    # Add your student checking logic here
-    return name.lower() in ['student1', 'student2', 'student3']
-
-if __name__ == "__main__":
-    user_name = input("Enter your name: ")
-    if is_student(user_name):
-        print(f"Hello {user_name}, welcome to the student area.")
+def factorial(n):
+# Calculate the factorial of a number.
+    if n == 0:
+        return 1
     else:
-        print(f"Sorry, {user_name}, you are not a student.")
+        return n * factorial(n- 1)
+        
+def main():
+    num = int(input("Enter a nonnegative integer: "))
+    print("The factorial of",num,"is",factorial(num))
+main()
